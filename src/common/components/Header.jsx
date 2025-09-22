8f72ec17c0bfec6cef2682e51dbddd2abf17e7a0
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-09-22 12:53:01 +0200

    Feat: add header

--- src/common/components/Header.jsx
+++ src/common/components/Header.jsx
@@ -1,23 +1,10 @@
-import { NavLink } from 'react-router'
-
-const nav = [
-    { path: "/", name: "Home" },
-    { path: "/about", name: "About" },
-    { path: "/contact", name: "Contact" },
-]
-
 export const Header = () => {
 
     return (
-        <header className="bg-blue-600 text-white p-4">
-            <div className="max-w-md mx-auto flex justify-between items-center">
-                <h1 className="text-xl font-bold">Todo list</h1>
+        <header className="p-4">
+            <div className="max-w-6xl px-4 mx-auto flex justify-between items-center">
+                <h1 className="text-xl font-bold">Where in the world?</h1>
                 <div className="flex items-center gap-4">
-                    {
-                        nav?.map((item, index) => (
-                            <NavLink key={index} className={({isActive}) => isActive ? 'font-bold' : 'font-light'} to={item.path}>{item.name}</NavLink>
-                        ))
-                    }
                 </div>
             </div>
         </header>
